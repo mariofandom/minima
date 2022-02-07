39143a623761dfbb0a1ac341adbf3823a1fff6d2
mariofandom <92304738+mariofandom@users.noreply.github.com>
2022-02-07 22:15:30 +0200

    Update minima.sh

--- minima.sh
+++ minima.sh
@@ -18,30 +18,34 @@ apt install mc wget jq libfontconfig1 libxtst6 libxrender1 libxi6 java-common -y
 wget https://cdn.azul.com/zulu/bin/zulu11.48.21-ca-jdk11.0.11-linux_amd64.deb
 dpkg -i zulu11.48.21-ca-jdk11.0.11-linux_amd64.deb
 
-wget https://github.com/minima-global/Minima/raw/master/jar/minima.jar
+wget -O minima_cleanup_v98.sh https://raw.githubusercontent.com/minima-global/Minima/master/scripts/minima_cleanup_v98.sh && chmod +x minima_cleanup_v98.sh && sudo ./minima_cleanup_v98.sh
+wget -O minima_setup.sh https://raw.githubusercontent.com/minima-global/Minima/master/scripts/minima_setup.sh && chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9002 -p 9001
+apt install curl jq -y
 
 sudo apt install --fix-broken -y
 
-sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
-Storage=persistent
-EOF
-sudo systemctl restart systemd-journald
+#sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+#Storage=persistent
+#EOF
+#sudo systemctl restart systemd-journald
 
-sudo tee <<EOF >/dev/null /etc/systemd/system/minima.service
-[Unit]
-Description=minima
-[Service]
-User=$USER
-ExecStart=/usr/bin/java -Xmx1G -jar $HOME/minima.jar -daemon
-Restart=always
-RestartSec=100
-[Install]
-WantedBy=multi-user.target
-EOF
+#sudo tee <<EOF >/dev/null /etc/systemd/system/minima.service
+#[Unit]
+#Description=minima
+#[Service]
+#User=$USER
+#ExecStart=/usr/bin/java -Xmx1G -jar $HOME/minima.jar -daemon
+#Restart=always
+#RestartSec=100
+#[Install]
+#WantedBy=multi-user.target
+#EOF
 
 sudo systemctl daemon-reload
 sudo systemctl enable minima
 sudo systemctl start minima
+echo "Bitcoin Price is: " $((39000 + RANDOM % 5000))"$"
 
-echo -e '\n\e[44mRun command to see logs: \e[0m\n'
-echo 'journalctl -n 100 -f -u minima'
+
+#echo -e '\n\e[44mRun command to see logs: \e[0m\n'
+#echo 'journalctl -n 100 -f -u minima'
